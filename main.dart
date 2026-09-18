52859664282230778ce97c3d01b174a2883c8734
iurigonzaga-png <iuri.gonzaga@esamcuberlandia.com.br>
2026-09-18 20:34:04 -0300

    Initialize Flutter app with animation screens

---
+++ main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'screens/animation_screen.dart';
+import 'screens/opacity_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Animações',
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Animações"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AnimationScreen(),
+                  ),
+                );
+              },
+              child: const Text("AnimatedContainer"),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const OpacityScreen(),
+                  ),
+                );
+              },
+              child: const Text("AnimatedOpacity"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
